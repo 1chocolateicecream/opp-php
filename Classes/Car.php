f8de3cbff028f6d389bee3ebe5682521b767903f
pipisa <your@email.ex>
2025-04-08 14:32:35 +0300

    Instantiate objects in OOP PHP + Access properties and methods in OOP PHP! new Car thing

--- Classes/Car.php
+++ Classes/Car.php
@@ -8,9 +8,12 @@ class Car
     private $vehicleType = "car";
 
     // Constructor
-    public function __construct($brand, $color)
+    public function __construct($brand, $color = "none")
     {
         $this->brand = $brand;
         $this->color = $color;
     }
 }
+
+$car01 = new Car("Volvo", "green");
+echo $car01->vehicleType;
